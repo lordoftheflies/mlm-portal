3084b6a46c6eb7b928688d73a2a434180562e40f
lordoftheflies <heglas11@gmail.com>
2016-06-27 14:43:35 +0200

    Extract subscription ID

--- service-worker.js
+++ service-worker.js
@@ -8,3 +8,47 @@
  * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
  */
 console.info('Service worker disabled for development, will be generated at build time.');
+console.info('But, now I\'m implementing push notifications.');
+
+
+console.log('Service worker for notifications started.', self);
+self.addEventListener('install', function (event) {
+    self.skipWaiting();
+    console.log('Notification service-worker installed', event);
+});
+self.addEventListener('activate', function (event) {
+    console.log('Notification service-worker  activated', event);
+});
+self.addEventListener('push', function (event) {
+    console.log('Notification service-worker received a push message.', event);
+    var title = 'Push message';
+    var payload = event.data ? event.data.text() : 'no payload';
+    event.waitUntil(self.registration.showNotification(title, {
+        body: payload,
+        icon: 'images/favicon-64x64.png',
+        tag: 'topflavon-notification'
+    }));
+});
+self.addEventListener('notificationclick', function (event) {
+    console.log('Notification click: tag ', event.notification.tag);
+    event.notification.close();
+    var url = '/content-view/ROOT';
+    event.waitUntil(clients.matchAll({
+        type: 'window'
+    })
+            .then(function (windowClients) {
+                for (var i = 0; i < windowClients.length; i++) {
+                    var client = windowClients[i];
+                    if (client.url === url && 'focus' in client) {
+                        return client.focus();
+                    }
+                }
+                if (clients.openWindow) {
+                    return clients.openWindow(url);
+                }
+            })
+            .catch(function (error) {
+                console.log('Notification click failed: ', error);
+            }));
+});
+
