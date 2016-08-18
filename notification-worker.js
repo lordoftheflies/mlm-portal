9b260684ceb05d55e222301c6940f606d1919f90
lordoftheflies <heglas11@gmail.com>
2016-08-18 12:56:27 +0200

    Notification worer.

--- notification-worker.js
+++ notification-worker.js
@@ -8,8 +8,8 @@
  * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
  */
 //var hostName = 'http://localhost:8084/topflavon-backend';
-var backendUrl = 'https://cherubits.hu:8443';
-var baseUrl = 'https://cherubits.hu/topflavon';
+var backendUrl = 'https://topflavon.net:8443';
+var baseUrl = 'https://www.topflavon.net';
 //var hostName = 'https://localhost:8443/topflavon-backend';
 //var hostName = 'http://185.51.67.30:8080/topflavon-backend';
 //var hostName = 'https://185.51.67.30:8443/topflavon-backend';
